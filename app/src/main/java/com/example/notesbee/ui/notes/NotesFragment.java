9d766aa993e4f681166489756e7ef8b458473ac8
ASMITA <asmita.kotkar@student.ufv.ca>
2021-03-27 22:38:09 -0700

    Added add notes button in notes fragment, design bug fix, added activity for adding notes -Asmita

--- app/src/main/java/com/example/notesbee/ui/notes/NotesFragment.java
+++ app/src/main/java/com/example/notesbee/ui/notes/NotesFragment.java
@@ -1,5 +1,6 @@
 package com.example.notesbee.ui.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.notesbee.AddNotesActivity;
 import com.example.notesbee.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NotesFragment extends Fragment {
 
@@ -20,11 +23,22 @@ public class NotesFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         NotesViewModel notesViewModel = new ViewModelProvider(this).get(NotesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notes, container, false);
-        final TextView textView = root.findViewById(R.id.text_notes);
+        //final TextView textView = root.findViewById(R.id.text_notes);
+        final FloatingActionButton addNotes = root.findViewById(R.id.add_notes_button);
+        addNotes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AddNotesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
         notesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
