efc3324d502fc2eff7df0878c4d3c5cc80589931
Paolo Mazzon <pdmazzon@gmail.com>
2021-04-03 15:56:29 -0700

    commented out code that was preventing compilation and added (very basic) alarm button that has not yet been implemented

--- app/src/main/java/com/example/notesbee/AddNotesActivity.java
+++ app/src/main/java/com/example/notesbee/AddNotesActivity.java
@@ -2,8 +2,11 @@ package com.example.notesbee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -44,6 +47,26 @@ public class AddNotesActivity extends AppCompatActivity {
         addDataToDatabase();
     }
 
+    /**
+     * Function called when the time is chosen from the alarm.
+     */
+    public void onTimeChanged(TimePicker view, int hour, int minute) {
+        System.out.println(hour + ":" + minute);
+    }
+
+    /**
+     * The function that is called when the "Alarm" button is pressed.
+     * It will launch a dialogue to select a time and schedule an alarm for that time.
+     */
+    public void setAlarm(View view) {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(view.getContext(), this::onTimeChanged, 0, 0, false);
+        timePickerDialog.show();
+    }
+
+    /**
+     * Adds the message currently written to the database, called whenever something would
+     * cause this message screen to close.
+     */
     private void addDataToDatabase(){
         // We will save all data in a note class then serialize it
         Note note = new Note();
